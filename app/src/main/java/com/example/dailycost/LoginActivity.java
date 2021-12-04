000fb6e5b27ac54174d6d394f6c1fc80be7a3317
itsdove <lianximing@icloud.com>
2021-12-04 17:07:43 +0800

    1

---
+++ app/src/main/java/com/example/dailycost/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.dailycost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginlayout);
+        TextView username=findViewById(R.id.username);
+        TextView password=findViewById(R.id.password);
+        Button button=findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().length()!=0&&password.getText().toString().length()!=0)
+                {Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);}
+                else
+                    Toast.makeText(getApplicationContext(),"请输入账号和密码",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
